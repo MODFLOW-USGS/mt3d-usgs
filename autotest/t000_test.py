71f2277b9baeabc4251dda399afeff7039840d9a
Christian Langevin <langevin@usgs.gov>
2018-12-14 13:41:58 -0500

    Working to get mt3dms compiling on Travis.  Case issue with FILESPEC.INC.

--- autotest/t000_test.py
+++ autotest/t000_test.py
@@ -5,6 +5,35 @@ import pymake
 import config
 
 
+def update_mt3dfiles(srcdir):
+    # Replace the getcl command with getarg
+    f1 = open(os.path.join(srcdir, 'mt3dms5.for'), 'r')
+    f2 = open(os.path.join(srcdir, 'mt3dms5.for.tmp'), 'w')
+    for line in f1:
+        f2.write(line.replace('CALL GETCL(FLNAME)', 'CALL GETARG(1,FLNAME)'))
+    f1.close()
+    f2.close()
+    os.remove(os.path.join(srcdir, 'mt3dms5.for'))
+    shutil.move(os.path.join(srcdir, 'mt3dms5.for.tmp'),
+                os.path.join(srcdir, 'mt3dms5.for'))
+
+    # Replace filespec with standard fortran
+    l = '''
+          CHARACTER*20 ACCESS,FORM,ACTION(2)
+          DATA ACCESS/'STREAM'/
+          DATA FORM/'UNFORMATTED'/
+          DATA (ACTION(I),I=1,2)/'READ','READWRITE'/
+    '''
+    fn = os.path.join(srcdir, 'FILESPEC.INC')
+    if os.path.isfile(fn):
+    	os.remove(fn)
+    f = open(fn, 'w')
+    f.write(l)
+    f.close()
+
+    return
+
+
 def test_compile_dev():
     # Compile development version of the program from source.
 
@@ -36,17 +65,8 @@ def test_compile_ref():
     # Copy MT3DMS into our test folder
     shutil.copytree(config.loc_release, dir_release)
 
-    # Replace the getcl command with getarg
-    print(config.srcdir_release)
-    f1 = open(os.path.join(config.srcdir_release, 'mt3dms5.for'), 'r')
-    f2 = open(os.path.join(config.srcdir_release, 'mt3dms5.for.tmp'), 'w')
-    for line in f1:
-        f2.write(line.replace('CALL GETCL(FLNAME)', 'CALL GETARG(1,FLNAME)'))
-    f1.close()
-    f2.close()
-    os.remove(os.path.join(config.srcdir_release, 'mt3dms5.for'))
-    shutil.move(os.path.join(config.srcdir_release, 'mt3dms5.for.tmp'),
-                os.path.join(config.srcdir_release, 'mt3dms5.for'))
+		# Modify the MT3D source files so that it compiles properly
+    update_mt3dfiles(srcdir)
 
     # compile
     pymake.main(srcdir, target, config.fc, 'gcc', makeclean=True,
