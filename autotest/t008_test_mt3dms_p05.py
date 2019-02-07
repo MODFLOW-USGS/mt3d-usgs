e5dc5193d3f7d074cdba81e1707883cb8d1d275a
GS\emorway-usgs <emorway@usgs.gov>
2019-02-07 14:27:58 -0800

    Expanding autotests with problem 5 from MT3DMS manual (pg 140: "Two-dimensional transport in a radial flow field")

--- autotest/t008_test_mt3dms_p05.py
+++ autotest/t008_test_mt3dms_p05.py
@@ -24,62 +24,50 @@ exe_name_mf = config.target_dict['mf2005']
 exe_name_mt3dms = config.target_dict['mt3dms']
 exe_name_mt3dusgs = os.path.abspath(config.target)
 testdir = './temp'
-testgroup = 'mt3dms_p04'
+testgroup = 'mt3dms_p05'
 
-def p04mt3d(exe_name_mf, exe_name_mt, model_ws, mixelm):
+def p05mt3d(exe_name_mf, exe_name_mt, model_ws, mixelm, dt0, ttsmult):
             	
     nlay = 1
-    nrow = 100
-    ncol = 100
+    nrow = 31
+    ncol = 31
     delr = 10
     delc = 10
     delv = 1
-    Lx = (ncol - 1) * delr
-    Ly = (nrow - 1) * delc
-    Ls = np.sqrt(Lx ** 2 + Ly ** 2)
-    v = 1.
-    prsity = 0.14
-    q = v * prsity
-    al = 2.
-    trpt = .1
-    q0 = 0.01
-    c0 = 1000.
+    prsity = 0.30
+    al = 10.
+    trpt = 1.
+    q0 = 100.
+    c0 = 1.
 
-    perlen_mf = 1000.
-    perlen_mt = 1000.
+    perlen_mf = 27.
+    perlen_mt = 27.
     hk = 1.
     laytyp = 0
 
-    modelname_mf = 'p04_mf'
-    mf = flopy.modflow.Modflow(modelname=modelname_mf, model_ws=model_ws, exe_name=exe_name_mf)
+    modelname_mf = 'p05_mf'
+    mf = flopy.modflow.Modflow(modelname=modelname_mf, model_ws=model_ws, 
+                               exe_name=exe_name_mf)
     dis = flopy.modflow.ModflowDis(mf, nlay=nlay, nrow=nrow, ncol=ncol,
                                    delr=delr, delc=delc, top=0., botm=[0 - delv],
                                    perlen=perlen_mf)
     ibound = np.ones((nlay, nrow, ncol), dtype=np.int) * -1
     ibound[:, 1:nrow - 1, 1:ncol-1] = 1
-
-    # set strt as a linear gradient at a 45 degree angle
-    h1 = q * Ls
-    x = dis.sr.xcentergrid
-    y = dis.sr.ycentergrid
-    a = -1
-    b = -1
-    c = 1
-    d = abs(a*x + b*y + c) / np.sqrt(2)
-    strt = h1 - d / Ls * h1
+    strt = 0.
 
     bas = flopy.modflow.ModflowBas(mf, ibound=ibound, strt=strt)
     lpf = flopy.modflow.ModflowLpf(mf, hk=hk, laytyp=laytyp)
-    wel = flopy.modflow.ModflowWel(mf, stress_period_data=[[0, 79, 20, q0]])
-    pcg = flopy.modflow.ModflowPcg(mf)
+    wel = flopy.modflow.ModflowWel(mf, stress_period_data=[[0, 15, 15, q0]])
+    sip = flopy.modflow.ModflowSip(mf)
     lmt = flopy.modflow.ModflowLmt(mf)
     mf.write_input()
     mf.run_model(silent=True)
 
-    modelname_mt = 'p04_mt'
+    modelname_mt = 'p05_mt'
     mt = flopy.mt3d.Mt3dms(modelname=modelname_mt, model_ws=model_ws, 
                            exe_name=exe_name_mt, modflowmodel=mf)
-    btn = flopy.mt3d.Mt3dBtn(mt, icbund=1, prsity=prsity, sconc=0)
+    btn = flopy.mt3d.Mt3dBtn(mt, icbund=1, prsity=prsity, sconc=0, 
+                             dt0=dt0, ttsmult=ttsmult)
     dceps = 1.e-5
     nplane = 1
     npl = 0
@@ -93,7 +81,7 @@ def p04mt3d(exe_name_mf, exe_name_mt, model_ws, mixelm):
                              npl=npl, nph=nph, npmin=npmin, npmax=npmax,
                              nlsink=nlsink, npsink=npsink, percel=0.5)
     dsp = flopy.mt3d.Mt3dDsp(mt, al=al, trpt=trpt)
-    spd = {0:[0, 79, 20, c0, 2]}
+    spd = {0:[0, 15, 15, c0, -1]}
     ssm = flopy.mt3d.Mt3dSsm(mt, stress_period_data=spd)
     gcg = flopy.mt3d.Mt3dGcg(mt)
     mt.write_input()
@@ -116,65 +104,32 @@ def p04mt3d(exe_name_mf, exe_name_mt, model_ws, mixelm):
     fname = os.path.join(model_ws, 'MT3D001.MAS')
     mvt = mt.load_mas(fname)
 
-    return mf, mt, conc, cvt, mvt
+    return mf, mt, conc, cvt, mvt  
 
 
 
-def test_mt3dmsp04a():  
+def test_mt3dmsp05a():  
     
-    mixelm = 1
+    mixelm  = -1
+    dt0     = 0.3
+    ttsmult = 1.0
 
 
     mt3dusgs_ws = os.path.join(testdir, testgroup + 'a')
-    mf, mt, conc_mt3dusgs, cvt, mvt = p04mt3d(exe_name_mf, exe_name_mt3dusgs,
-                                              mt3dusgs_ws, mixelm)
+    mf, mt, conc_mt3dusgs, cvt, mvt = p05mt3d(exe_name_mf, exe_name_mt3dusgs,
+                                              mt3dusgs_ws, mixelm, dt0, ttsmult)
 
     mt3dms_ws = os.path.join(mt3dusgs_ws, 'mt3dms')
-    mf, mt, conc_mt3dms, cvt, mvt = p04mt3d(exe_name_mf, exe_name_mt3dms,
-                                            mt3dms_ws, mixelm)
+    mf, mt, conc_mt3dms, cvt, mvt = p05mt3d(exe_name_mf, exe_name_mt3dms,
+                                            mt3dms_ws, mixelm, dt0, ttsmult)
 
     msg = 'concentrations not equal {} {}'.format(conc_mt3dusgs, conc_mt3dms)
-    assert  np.allclose(conc_mt3dusgs, conc_mt3dms, atol=6.0e-4), msg
+    assert  np.allclose(conc_mt3dusgs, conc_mt3dms, atol=1.0e-4), msg
     return
 
-
-def test_mt3dmsp04b(): 
-    
-    mixelm = 0
-
-    mt3dusgs_ws = os.path.join(testdir, testgroup + 'b')
-    mf, mt, conc_mt3dusgs, cvt, mvt = p04mt3d(exe_name_mf, exe_name_mt3dusgs,
-                                              mt3dusgs_ws, mixelm)
-
-    mt3dms_ws = os.path.join(mt3dusgs_ws, 'mt3dms')
-    mf, mt, conc_mt3dms, cvt, mvt = p04mt3d(exe_name_mf, exe_name_mt3dms,
-                                            mt3dms_ws, mixelm)
-
-    msg = 'concentrations not equal {} {}'.format(conc_mt3dusgs, conc_mt3dms)
-    assert np.allclose(conc_mt3dusgs, conc_mt3dms, atol=1e-4), msg
-    return
-
-
-def test_mt3dmsp04c():
-    mixelm = -1
-
-    mt3dusgs_ws = os.path.join(testdir, testgroup + 'b')
-    mf, mt, conc_mt3dusgs, cvt, mvt = p04mt3d(exe_name_mf, exe_name_mt3dusgs,
-                                              mt3dusgs_ws, mixelm)
-
-    mt3dms_ws = os.path.join(mt3dusgs_ws, 'mt3dms')
-    mf, mt, conc_mt3dms, cvt, mvt = p04mt3d(exe_name_mf, exe_name_mt3dms,
-                                            mt3dms_ws, mixelm)
-
-    msg = 'concentrations not equal {} {}'.format(conc_mt3dusgs, conc_mt3dms)
-    assert np.allclose(conc_mt3dusgs, conc_mt3dms, atol=1e-4), msg
-    return
 
 
 if __name__ == "__main__":
     # print message
     print('standalone run of {}'.format(os.path.basename(__file__)))
-    test_mt3dmsp04a()
-    test_mt3dmsp04b()
-    test_mt3dmsp04c()
-
+    test_mt3dmsp05a()
